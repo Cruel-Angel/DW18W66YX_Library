efec6e2980304f8d1c1a9d490e9a634daca50999
Rizky Iqbal <rizkiiqbal36@gmail.com>
2020-11-02 06:54:18 +0700

    gaskeun

--- src/config/api.js
+++ src/config/api.js
@@ -5,21 +5,21 @@ export const setAuthToken = (token) => {
   else delete API.defaults.headers.common["Authorization"];
 };
 //dev
-export const urlAsset = {
+/*export const urlAsset = {
   avatar: "http://localhost:5000/public/avatars/",
   thumbnail: "http://localhost:5000/public/thumbnails/",
   file: "http://localhost:5000/public/files/",
 };
 export const API = axios.create({
   baseURL: "http://localhost:5000/api/v1",
-});
+});*/
 
 //production
-/*export const urlAsset = {
+export const urlAsset = {
   avatar: "https://library-api-rizky-iqbal.herokuapp.com/public/avatars/",
   thumbnail: "https://library-api-rizky-iqbal.herokuapp.com/public/thumbnails/",
   file: "https://library-api-rizky-iqbal.herokuapp.com/public/files/",
 };
 export const API = axios.create({
   baseURL: "https://library-api-rizky-iqbal.herokuapp.com/api/v1",
-});*/
+});
